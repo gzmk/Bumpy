1ed558f486ba369fef8c4058182d01d6cb088059
Gizem Kucukoglu <gizemkucukoglu@gmail.com>
2014-11-11 17:03:09 -0500

    Render toolbox script for rendering 5 levels of bumpy half-ellipsoid arrays

---
+++ render_bumpy.m
@@ -0,0 +1,181 @@
+% Author: Gizem Kucukoglu
+% Nov 2014
+
+% Initialize; 
+clear; close all; clc; 
+
+%% Render bump level 1 = 0.4cm
+% Set preferences
+setpref('RenderToolbox3', 'workingFolder', '/users2/gizem/Documents/Research/Bumpy');
+
+% use this scene and condition file. 
+parentSceneFile = 'bumpy1_hemilight.dae';
+
+mappingsFile = 'bumpy1_hemilightDefaultMappings.txt';
+
+% Make sure all illuminants are added to the path. 
+addpath(genpath(pwd))
+
+
+% Choose batch renderer options.
+
+hints.imageWidth = 960;
+hints.imageHeight = 960;
+hints.renderer = 'Mitsuba';
+hints.recipeName = ['Test-' date];
+
+
+ChangeToWorkingFolder(hints);
+
+nativeSceneFiles = MakeSceneFiles(parentSceneFile, '', mappingsFile, hints);
+radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+
+%comment all this out
+toneMapFactor = 10;
+isScale = true;
+montageName = sprintf('%s (%s)', 'BumpyTestHDR', hints.renderer);
+montageFile = [montageName '.png'];
+[SRGBMontage, XYZMontage] = ...
+    MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+ShowXYZAndSRGB([], SRGBMontage, montageName);
+
+%% Render bump level 2 = 0.9cm
+% Set preferences
+setpref('RenderToolbox3', 'workingFolder', '/users2/gizem/Documents/Research/Bumpy');
+
+% use this scene and condition file. 
+parentSceneFile = 'bumpy1_hemilight.dae';
+
+mappingsFile = 'bumpy1_hemilightDefaultMappings.txt';
+
+% Make sure all illuminants are added to the path. 
+addpath(genpath(pwd))
+
+
+% Choose batch renderer options.
+
+hints.imageWidth = 960;
+hints.imageHeight = 960;
+hints.renderer = 'Mitsuba';
+hints.recipeName = ['Test-' date];
+
+
+ChangeToWorkingFolder(hints);
+
+nativeSceneFiles = MakeSceneFiles(parentSceneFile, '', mappingsFile, hints);
+radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+
+%comment all this out
+toneMapFactor = 10;
+isScale = true;
+montageName = sprintf('%s (%s)', 'BumpyTestHDR', hints.renderer);
+montageFile = [montageName '.png'];
+[SRGBMontage, XYZMontage] = ...
+    MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+ShowXYZAndSRGB([], SRGBMontage, montageName);
+
+%% Render bump level 3 = 1.6cm
+% Set preferences
+setpref('RenderToolbox3', 'workingFolder', '/users2/gizem/Documents/Research/Bumpy');
+
+% use this scene and condition file. 
+parentSceneFile = 'bumpy1_hemilight.dae';
+
+mappingsFile = 'bumpy1_hemilightDefaultMappings.txt';
+
+% Make sure all illuminants are added to the path. 
+addpath(genpath(pwd))
+
+
+% Choose batch renderer options.
+
+hints.imageWidth = 960;
+hints.imageHeight = 960;
+hints.renderer = 'Mitsuba';
+hints.recipeName = ['Test-' date];
+
+
+ChangeToWorkingFolder(hints);
+
+nativeSceneFiles = MakeSceneFiles(parentSceneFile, '', mappingsFile, hints);
+radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+
+%comment all this out
+toneMapFactor = 10;
+isScale = true;
+montageName = sprintf('%s (%s)', 'BumpyTestHDR', hints.renderer);
+montageFile = [montageName '.png'];
+[SRGBMontage, XYZMontage] = ...
+    MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+ShowXYZAndSRGB([], SRGBMontage, montageName);
+
+%% Render bump level 4 = 2.5cm
+% Set preferences
+setpref('RenderToolbox3', 'workingFolder', '/users2/gizem/Documents/Research/Bumpy');
+
+% use this scene and condition file. 
+parentSceneFile = 'bumpy1_hemilight.dae';
+
+mappingsFile = 'bumpy1_hemilightDefaultMappings.txt';
+
+% Make sure all illuminants are added to the path. 
+addpath(genpath(pwd))
+
+
+% Choose batch renderer options.
+
+hints.imageWidth = 960;
+hints.imageHeight = 960;
+hints.renderer = 'Mitsuba';
+hints.recipeName = ['Test-' date];
+
+
+ChangeToWorkingFolder(hints);
+
+nativeSceneFiles = MakeSceneFiles(parentSceneFile, '', mappingsFile, hints);
+radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+
+%comment all this out
+toneMapFactor = 10;
+isScale = true;
+montageName = sprintf('%s (%s)', 'BumpyTestHDR', hints.renderer);
+montageFile = [montageName '.png'];
+[SRGBMontage, XYZMontage] = ...
+    MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+ShowXYZAndSRGB([], SRGBMontage, montageName);
+
+%% Render bump level 5 = 3.6cm
+% Set preferences
+setpref('RenderToolbox3', 'workingFolder', '/users2/gizem/Documents/Research/Bumpy');
+
+% use this scene and condition file. 
+parentSceneFile = 'bumpy1_hemilight.dae';
+
+mappingsFile = 'bumpy1_hemilightDefaultMappings.txt';
+
+% Make sure all illuminants are added to the path. 
+addpath(genpath(pwd))
+
+
+% Choose batch renderer options.
+
+hints.imageWidth = 960;
+hints.imageHeight = 960;
+hints.renderer = 'Mitsuba';
+hints.recipeName = ['Test-' date];
+
+
+ChangeToWorkingFolder(hints);
+
+nativeSceneFiles = MakeSceneFiles(parentSceneFile, '', mappingsFile, hints);
+radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+
+%comment all this out
+toneMapFactor = 10;
+isScale = true;
+montageName = sprintf('%s (%s)', 'BumpyTestHDR', hints.renderer);
+montageFile = [montageName '.png'];
+[SRGBMontage, XYZMontage] = ...
+    MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+ShowXYZAndSRGB([], SRGBMontage, montageName);
+
