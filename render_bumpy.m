290cf1e045d1b3ea1e25a37df3b3f29174da512a
Gizem Kucukoglu <gizemkucukoglu@gmail.com>
2014-11-14 14:20:33 -0500

    For loop is added to render 5 material conditions for each bump level using the new mappings and conditions files

--- render_bumpy.m
+++ render_bumpy.m
@@ -20,29 +20,48 @@ addpath(genpath(pwd))
 hints.whichConditions = [];
 
 % Choose batch renderer options.
-hints.imageWidth = 960;
-hints.imageHeight = 960;
-hints.renderer = 'Mitsuba';
+hints.imageWidth = 480;
+hints.imageHeight = 480;
+% hints.renderer = 'Mitsuba';
 datetime=datestr(now);
 datetime=strrep(datetime,':','_'); %Replace colon with underscore
 datetime=strrep(datetime,'-','_');%Replace minus sign with underscore
 datetime=strrep(datetime,' ','_');%Replace space with underscore
 hints.recipeName = ['Test-Gloss-' datetime];
 
-
 ChangeToWorkingFolder(hints);
 
-nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
-radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+% nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
+% radianceDataFiles = BatchRender(nativeSceneFiles, hints);
 
 %comment all this out
 toneMapFactor = 10;
 isScale = true;
-montageName = sprintf('%s (%s)', 'Bump1', hints.renderer);
-montageFile = [montageName '.png'];
-[SRGBMontage, XYZMontage] = ...
-    MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
-ShowXYZAndSRGB([], SRGBMontage, montageName);
+
+for renderer = {'Mitsuba'}
+    
+    % choose one renderer
+    hints.renderer = renderer{1};
+    
+    % make 3 multi-spectral renderings, saved in .mat files
+    nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
+    radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+    
+    % condense multi-spectral renderings into one sRGB montage
+    montageName = sprintf('Bump1 (%s)', hints.renderer);
+    montageFile = [montageName '.png'];
+    [SRGBMontage, XYZMontage] = ...
+        MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+    
+    % display the sRGB montage
+    ShowXYZAndSRGB([], SRGBMontage, montageName);
+end
+
+% montageName = sprintf('Bump1 (%s)', hints.renderer);
+% montageFile = [montageName '.png'];
+% [SRGBMontage, XYZMontage] = ...
+%     MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+% ShowXYZAndSRGB([], SRGBMontage, montageName);
 
 %% Render bump level 2 = 0.9cm
 % Set preferences
@@ -62,7 +81,7 @@ hints.whichConditions = [];
 % Choose batch renderer options.
 hints.imageWidth = 960;
 hints.imageHeight = 960;
-hints.renderer = 'Mitsuba';
+% hints.renderer = 'Mitsuba';
 datetime=datestr(now);
 datetime=strrep(datetime,':','_'); %Replace colon with underscore
 datetime=strrep(datetime,'-','_');%Replace minus sign with underscore
@@ -71,17 +90,37 @@ hints.recipeName = ['Test-' datetime];
 
 ChangeToWorkingFolder(hints);
 
-nativeSceneFiles = MakeSceneFiles(parentSceneFile, '', mappingsFile, hints);
-radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+% nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
+% radianceDataFiles = BatchRender(nativeSceneFiles, hints);
 
 %comment all this out
 toneMapFactor = 10;
 isScale = true;
-montageName = sprintf('%s (%s)', 'Level2', hints.renderer);
-montageFile = [montageName '.png'];
-[SRGBMontage, XYZMontage] = ...
-    MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
-ShowXYZAndSRGB([], SRGBMontage, montageName);
+
+for renderer = {'Mitsuba'}
+    
+    % choose one renderer
+    hints.renderer = renderer{1};
+    
+    % make 3 multi-spectral renderings, saved in .mat files
+    nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
+    radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+    
+    % condense multi-spectral renderings into one sRGB montage
+    montageName = sprintf('Bump2 (%s)', hints.renderer);
+    montageFile = [montageName '.png'];
+    [SRGBMontage, XYZMontage] = ...
+        MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+    
+    % display the sRGB montage
+    ShowXYZAndSRGB([], SRGBMontage, montageName);
+end
+
+% montageName = sprintf('%s (%s)', 'Level2', hints.renderer);
+% montageFile = [montageName '.png'];
+% [SRGBMontage, XYZMontage] = ...
+%     MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+% ShowXYZAndSRGB([], SRGBMontage, montageName);
 
 %% Render bump level 3 = 1.6cm
 % Set preferences
@@ -101,7 +140,7 @@ hints.whichConditions = [];
 % Choose batch renderer options.
 hints.imageWidth = 960;
 hints.imageHeight = 960;
-hints.renderer = 'Mitsuba';
+% hints.renderer = 'Mitsuba';
 datetime=datestr(now);
 datetime=strrep(datetime,':','_'); %Replace colon with underscore
 datetime=strrep(datetime,'-','_');%Replace minus sign with underscore
@@ -110,17 +149,37 @@ hints.recipeName = ['Test-' datetime];
 
 ChangeToWorkingFolder(hints);
 
-nativeSceneFiles = MakeSceneFiles(parentSceneFile, '', mappingsFile, hints);
-radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+% nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
+% radianceDataFiles = BatchRender(nativeSceneFiles, hints);
 
 %comment all this out
 toneMapFactor = 10;
 isScale = true;
-montageName = sprintf('%s (%s)', 'Level3', hints.renderer);
-montageFile = [montageName '.png'];
-[SRGBMontage, XYZMontage] = ...
-    MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
-ShowXYZAndSRGB([], SRGBMontage, montageName);
+
+for renderer = {'Mitsuba'}
+    
+    % choose one renderer
+    hints.renderer = renderer{1};
+    
+    % make 3 multi-spectral renderings, saved in .mat files
+    nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
+    radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+    
+    % condense multi-spectral renderings into one sRGB montage
+    montageName = sprintf('Bump2 (%s)', hints.renderer);
+    montageFile = [montageName '.png'];
+    [SRGBMontage, XYZMontage] = ...
+        MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+    
+    % display the sRGB montage
+    ShowXYZAndSRGB([], SRGBMontage, montageName);
+end
+
+% montageName = sprintf('%s (%s)', 'Level3', hints.renderer);
+% montageFile = [montageName '.png'];
+% [SRGBMontage, XYZMontage] = ...
+%     MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+% ShowXYZAndSRGB([], SRGBMontage, montageName);
 
 %% Render bump level 4 = 2.5cm
 % Set preferences
@@ -140,7 +199,7 @@ hints.whichConditions = [];
 % Choose batch renderer options.
 hints.imageWidth = 960;
 hints.imageHeight = 960;
-hints.renderer = 'Mitsuba';
+% hints.renderer = 'Mitsuba';
 datetime=datestr(now);
 datetime=strrep(datetime,':','_'); %Replace colon with underscore
 datetime=strrep(datetime,'-','_');%Replace minus sign with underscore
@@ -149,17 +208,37 @@ hints.recipeName = ['Test-' datetime];
 
 ChangeToWorkingFolder(hints);
 
-nativeSceneFiles = MakeSceneFiles(parentSceneFile, '', mappingsFile, hints);
-radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+% nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
+% radianceDataFiles = BatchRender(nativeSceneFiles, hints);
 
 %comment all this out
 toneMapFactor = 10;
 isScale = true;
-montageName = sprintf('%s (%s)', 'Level4', hints.renderer);
-montageFile = [montageName '.png'];
-[SRGBMontage, XYZMontage] = ...
-    MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
-ShowXYZAndSRGB([], SRGBMontage, montageName);
+
+for renderer = {'Mitsuba'}
+    
+    % choose one renderer
+    hints.renderer = renderer{1};
+    
+    % make 3 multi-spectral renderings, saved in .mat files
+    nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
+    radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+    
+    % condense multi-spectral renderings into one sRGB montage
+    montageName = sprintf('Bump2 (%s)', hints.renderer);
+    montageFile = [montageName '.png'];
+    [SRGBMontage, XYZMontage] = ...
+        MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+    
+    % display the sRGB montage
+    ShowXYZAndSRGB([], SRGBMontage, montageName);
+end
+
+% montageName = sprintf('%s (%s)', 'Level4', hints.renderer);
+% montageFile = [montageName '.png'];
+% [SRGBMontage, XYZMontage] = ...
+%     MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+% ShowXYZAndSRGB([], SRGBMontage, montageName);
 
 %% Render bump level 5 = 3.6cm
 % Set preferences
@@ -177,9 +256,9 @@ addpath(genpath(pwd))
 hints.whichConditions = [];
 
 % Choose batch renderer options.
-hints.imageWidth = 960;
-hints.imageHeight = 960;
-hints.renderer = 'Mitsuba';
+hints.imageWidth = 480;
+hints.imageHeight = 480;
+% hints.renderer = 'Mitsuba';
 datetime=datestr(now);
 datetime=strrep(datetime,':','_'); %Replace colon with underscore
 datetime=strrep(datetime,'-','_');%Replace minus sign with underscore
@@ -188,15 +267,35 @@ hints.recipeName = ['Test-' datetime];
 
 ChangeToWorkingFolder(hints);
 
-nativeSceneFiles = MakeSceneFiles(parentSceneFile, '', mappingsFile, hints);
-radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+% nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
+% radianceDataFiles = BatchRender(nativeSceneFiles, hints);
 
 %comment all this out
 toneMapFactor = 10;
 isScale = true;
-montageName = sprintf('%s (%s)', 'Level5', hints.renderer);
-montageFile = [montageName '.png'];
-[SRGBMontage, XYZMontage] = ...
-    MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
-ShowXYZAndSRGB([], SRGBMontage, montageName);
+
+for renderer = {'Mitsuba'}
+    
+    % choose one renderer
+    hints.renderer = renderer{1};
+    
+    % make 3 multi-spectral renderings, saved in .mat files
+    nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
+    radianceDataFiles = BatchRender(nativeSceneFiles, hints);
+    
+    % condense multi-spectral renderings into one sRGB montage
+    montageName = sprintf('Bump2 (%s)', hints.renderer);
+    montageFile = [montageName '.png'];
+    [SRGBMontage, XYZMontage] = ...
+        MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+    
+    % display the sRGB montage
+    ShowXYZAndSRGB([], SRGBMontage, montageName);
+end
+
+% montageName = sprintf('%s (%s)', 'Level5', hints.renderer);
+% montageFile = [montageName '.png'];
+% [SRGBMontage, XYZMontage] = ...
+%     MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+% ShowXYZAndSRGB([], SRGBMontage, montageName);
 
